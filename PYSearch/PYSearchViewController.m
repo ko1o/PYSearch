4b43bb4889f7bfc2342c945578fbba07ff21ed25
Gang679 <xinshijie@xinshijietekiiMac.local>
2017-08-22 15:53:01 +0800

    适配Xcode9 iOS11版本
    搜索框右移，遮挡住取消按钮！

--- PYSearch/PYSearchViewController.m
+++ PYSearch/PYSearchViewController.m
@@ -117,8 +117,11 @@
         self.searchHistories = self.searchHistories;
         self.currentOrientation = [[UIDevice currentDevice] orientation];
     }
-    
-    self.navigationItem.titleView.py_x = PYSEARCH_MARGIN * 2;
+    if ( [[[UIDevice currentDevice] systemVersion] intValue] >= 11.0) {
+        self.navigationItem.titleView.py_x = PYSEARCH_MARGIN  * 0.5 ;
+    }else{
+        self.navigationItem.titleView.py_x = PYSEARCH_MARGIN  * 2 ;
+    }
     self.navigationItem.titleView.py_y = self.view.py_width > self.view.py_height ? 3 : 7;
     self.navigationItem.titleView.py_width = self.view.py_width - 44 - self.navigationItem.titleView.py_x * 2;
     self.navigationItem.titleView.py_height = self.view.py_width > self.view.py_height ? 24 : 30;
